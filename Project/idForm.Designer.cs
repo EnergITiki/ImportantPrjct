773739b55a7a10bdb6d95cfe4e033e67b92a2bc0
KirillBuiko <k.buiko04@mail.ru>
2020-10-23 15:01:22 +0300

    Добавил старый проект

---
+++ Project/idForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace window3
+{
+    partial class idForm
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(idForm));
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.expLabel = new System.Windows.Forms.Label();
+            this.mailLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.sexLabel = new System.Windows.Forms.Label();
+            this.statLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameLabel.ForeColor = System.Drawing.Color.White;
+            this.nameLabel.Location = new System.Drawing.Point(28, 25);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(51, 19);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "ФИО: ";
+            // 
+            // expLabel
+            // 
+            this.expLabel.AutoSize = true;
+            this.expLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.expLabel.ForeColor = System.Drawing.Color.White;
+            this.expLabel.Location = new System.Drawing.Point(28, 60);
+            this.expLabel.Name = "expLabel";
+            this.expLabel.Size = new System.Drawing.Size(54, 19);
+            this.expLabel.TabIndex = 3;
+            this.expLabel.Text = "Стаж: ";
+            // 
+            // mailLabel
+            // 
+            this.mailLabel.AutoSize = true;
+            this.mailLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mailLabel.ForeColor = System.Drawing.Color.White;
+            this.mailLabel.Location = new System.Drawing.Point(28, 94);
+            this.mailLabel.Name = "mailLabel";
+            this.mailLabel.Size = new System.Drawing.Size(57, 19);
+            this.mailLabel.TabIndex = 4;
+            this.mailLabel.Text = "Почта: ";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ageLabel.ForeColor = System.Drawing.Color.White;
+            this.ageLabel.Location = new System.Drawing.Point(28, 127);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(74, 19);
+            this.ageLabel.TabIndex = 5;
+            this.ageLabel.Text = "Возраст: ";
+            // 
+            // sexLabel
+            // 
+            this.sexLabel.AutoSize = true;
+            this.sexLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sexLabel.ForeColor = System.Drawing.Color.White;
+            this.sexLabel.Location = new System.Drawing.Point(28, 160);
+            this.sexLabel.Name = "sexLabel";
+            this.sexLabel.Size = new System.Drawing.Size(44, 19);
+            this.sexLabel.TabIndex = 6;
+            this.sexLabel.Text = "Пол: ";
+            // 
+            // statLabel
+            // 
+            this.statLabel.AutoSize = true;
+            this.statLabel.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statLabel.ForeColor = System.Drawing.Color.White;
+            this.statLabel.Location = new System.Drawing.Point(28, 192);
+            this.statLabel.Name = "statLabel";
+            this.statLabel.Size = new System.Drawing.Size(93, 19);
+            this.statLabel.TabIndex = 7;
+            this.statLabel.Text = "Должность: ";
+            // 
+            // idForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
+            this.ClientSize = new System.Drawing.Size(494, 249);
+            this.Controls.Add(this.statLabel);
+            this.Controls.Add(this.sexLabel);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.mailLabel);
+            this.Controls.Add(this.expLabel);
+            this.Controls.Add(this.nameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "idForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Личный кабинет";
+            this.Load += new System.EventHandler(this.idForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label expLabel;
+        private System.Windows.Forms.Label mailLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Label sexLabel;
+        private System.Windows.Forms.Label statLabel;
+    }
+}
---
+++ Project/mainForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace window3
+{
+
+    public partial class mainForm : Form
+    {
+        bool flag = false;
+        regForm reg = new regForm();
+        loginForm login = new loginForm();
+        idForm idform = new idForm();
+        user curuser = new user();
+        teleForm tele = new teleForm();
+        mapForm map = new mapForm();
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+        
+        //Закрытие формы входа
+        public void loginForm_FormClosed(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            mapButton.Visible = false;
+        }
+
+        //Разворачивание из трея при двойной нажатии на иконку
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        //Сворачивание приложения в трей при нажатии на крестик
+        public void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            this.ShowInTaskbar = false;
+            e.Cancel = true;
+            if (flag == true)
+            {
+                e.Cancel = false;
+                flag = false;
+            }
+        }
+        
+        //При нажатии на кнопку открытие формы регистрации 
+
+        //При входе в учетную запись на главном экране переопределяются кнопки
+        //"Вход" => "Телеметрия"
+        //"Регистрация" => "Личный кабинет"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            login.ShowDialog();          
+            if (login.result.sex < 3)
+            {
+                curuser = login.result;
+                
+                buttonEnter.Text = "Телеметрия";
+                buttonEnter.Click -= button1_Click;
+                buttonEnter.Click += new System.EventHandler(next_button1_Click);
+
+                idButton.Text = "Личный кабинет";
+                idButton.Click += new System.EventHandler(idButton_Click_1);
+                idButton.Visible = true;
+                mapButton.Visible = true;
+                if(curuser.perm_level==3)
+                    regButton.Visible = true;
+                nameLabel.Text = curuser.name;
+            }
+            this.Show();
+        }
+
+        //При нажатии открывается форма Телеметрия
+        private void next_button1_Click(object sender, EventArgs e)
+        {
+            tele.curuser = curuser;
+            tele.ShowDialog();   
+        }
+
+        //Завершение работы из главного экрана 
+        private void ВыходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            this.Close();
+        }
+        
+        //При нажатии открывается личный кабинет при условии входа в учетную запись
+        private void idButton_Click_1(object sender, EventArgs e)
+        {
+            idform.curuser = curuser;
+            idform.ShowDialog();
+        }
+
+        //Завершение работы из трея
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            this.Close();
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            map.ShowDialog();
+        }
+
+        private void Button2_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            reg.curuser = curuser;
+            reg.ShowDialog();
+            this.Show();
+        }
+    }
+}
